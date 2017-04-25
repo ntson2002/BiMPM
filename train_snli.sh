d597743885ad6fea8f62b663749e2c51bf626885
Son Nguyen <ntson2002@gmail.com>
2017-04-25 10:57:28 +0900

    add sh file for running script

--- train_snli.sh
+++ train_snli.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+EMB=/work/sonnguyen/glove/glove.42B.300d.txt
+
+FOLDER=/work/sonnguyen/snli/snli_1.0
+TRAIN=$FOLDER/snli_1.0_train.txt
+DEV=$FOLDER/snli_1.0_dev.txt
+TEST=$FOLDER/snli_1.0_test.txt
+
+python ./src/SentenceMatchTrainer.py --train_path $TRAIN --dev_path $DEV --test_path $TEST --word_vec_path $EMB --suffix sample --fix_word_vec --model_dir models/ --MP_dim 20
